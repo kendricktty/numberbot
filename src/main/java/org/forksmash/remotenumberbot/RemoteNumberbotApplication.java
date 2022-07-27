61580f1c2edab6324cdcebe9329f3dab7ad4c8bd
kendricktty <79306624+kendricktty@users.noreply.github.com>
2022-07-27 18:10:22 +0800

    first commit

---
+++ src/main/java/org/forksmash/remotenumberbot/RemoteNumberbotApplication.java
@@ -0,0 +1,13 @@
+package org.forksmash.remotenumberbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteNumberbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteNumberbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/forksmash/remotenumberbot/RemoteNumberbotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.forksmash.remotenumberbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteNumberbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
