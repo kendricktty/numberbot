839cb44b14ad0c75b18246a606429643160c08d0
kendricktty <79306624+kendricktty@users.noreply.github.com>
2022-07-27 23:57:28 +0800

    Created processors and generators

--- src/main/java/org/forksmash/remotenumberbot/utility/generator/RandomNumberGenerator.java
+++ src/main/java/org/forksmash/remotenumberbot/utility/generator/RandomNumberGenerator.java
@@ -6,10 +6,9 @@ public class RandomNumberGenerator extends Generator {
     @Override
     public int generate(int pow) {
         Random random = new Random();
-        int result = random.nextInt(10 - 1) + 1;
-        for (int i = 1; i <= pow; i++) {
+        for (int i = 0; i <= pow; i++) {
             random = new Random();
-            int randomNumber = random.nextInt(10);
+            int randomNumber = i == pow ? random.nextInt(10 - 1) + 1 : random.nextInt(10);
             result += ((int) Math.pow(10, i) * randomNumber);
         }
         return result;
