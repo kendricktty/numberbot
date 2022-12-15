ae2ca914aa170fc3e5a5861b00b4756356002e13
kendricktty <79306624+kendricktty@users.noreply.github.com>
2022-12-15 22:43:50 +0800

    Changed method used to retrieve bot token and username

--- src/main/java/org/forksmash/remotenumberbot/RemoteNumberbotApplication.java
+++ src/main/java/org/forksmash/remotenumberbot/RemoteNumberbotApplication.java
@@ -1,14 +1,16 @@
 package org.forksmash.remotenumberbot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class RemoteNumberbotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RemoteNumberbotApplication.class, args);
+		new SpringApplicationBuilder(RemoteNumberbotApplication.class).web(WebApplicationType.SERVLET).run(args);
 	}
 
 }
