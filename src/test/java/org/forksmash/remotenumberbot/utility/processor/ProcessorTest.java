a42318ddd8b3cb4af4f7d9b2c931a2732a3765ff
kendricktty <79306624+kendricktty@users.noreply.github.com>
2025-01-08 12:13:59 +0800

    Processor unit tests

--- src/test/java/org/forksmash/remotenumberbot/utility/processor/ProcessorTest.java
+++ src/test/java/org/forksmash/remotenumberbot/utility/processor/ProcessorTest.java
@@ -37,7 +37,18 @@ public class ProcessorTest {
         String response = messageProcessor.processRequest(request);
         assertTrue(response.contains("Here's your generated random number!"));
         assertTrue(response.contains("The number returned is <b>123</b>."));
+        // Each when() statement should be followed up with a verify() to verify that the mocked method was performed
+        verify(randomGenerator).generate(3);
+    }
 
+    @Test
+    public void processRequest_p2gHappy() throws BotException {
+        String request = "/p 3";
+        when(power2Generator.generate(3)).thenReturn(256L);
+        String response = messageProcessor.processRequest(request);
+        assertTrue(response.contains("Here's your power of 2!"));
+        assertTrue(response.contains("The number returned is <b>256</b>."));
+        verify(power2Generator).generate(3);
     }
 
     @Test
@@ -49,10 +60,11 @@ public class ProcessorTest {
     }
 
     @Test
-    public void processRequest_() {
+    public void processRequest_invalid() {
         BotException e = assertThrows(InvalidInputException.class, () -> {
-            messageProcessor.processRequest("/r");
+            messageProcessor.processRequest("hello");
         });
-        assertEquals("Request incomplete. Type /r or /p, followed by a space, and then a number.", e.getMessage());
+        String invalidInputMessage = "Invalid input. Enter /r or /p, followed by a space, and then a number.";
+        assertEquals(invalidInputMessage, e.getMessage());
     }
 }
